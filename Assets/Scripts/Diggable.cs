15d28f666e74bdd160938d8f3cd32541197921bd
Edgardo Velazquez <edv9298@hotmail.com>
2022-03-20 02:00:04 -0400

    messing with scriptable objects

---
+++ Assets/Scripts/Diggable.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Diggable", menuName = "Custom/Diggable Object", order = 1   )]
+public class Diggable : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
