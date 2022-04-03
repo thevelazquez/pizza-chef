e1bcf592292cdaeaea91f68744441393efebffda
Edgardo Velazquez <edv9298@hotmail.com>
2022-04-02 20:24:09 -0400

    Started inventory template on code build scene

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class InventoryController : MonoBehaviour
 {
     public GameObject collectibles;
+    //GameObject Inventory;
 
     // Start is called before the first frame update
     void Start()
-    {
+    {   
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    void CreateListItems() {
         
     }
 }
