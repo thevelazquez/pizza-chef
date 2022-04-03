b53469c9fb26b494589700f1acdc40379cb29d01
April Dean <aprilldean@gmail.com>
2022-04-03 00:15:00 -0400

    working on pause

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,39 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-        public GameObject pauseCanvasMenu;
-        public bool isPaused;
+    public static bool isPaused = false;
+    public GameObject aboutScreen;
+    public GameObject creditScreen;
+    public GameObject controlScreen;
+    public GameObject genScreen;
+    public GameObject UIScreen;
+
     // Start is called before the first frame update
- 
+    void Start()
+    {
+        Resume();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("p"))
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (isPaused)
             {
-                Debug.Log("Test");
-                if(isPaused)
-                {
-                    CloseMenu();
-                }
-                else
-                {
-                        OpenMenu();
-                }
-        }
-        
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }        
     }
-public void OpenMenu()
+
+    public void Resume()
     {
-        pauseCanvasMenu.SetActive(true);
-        isPaused = true;
-    }
-public void CloseMenu()
-    {
-        pauseCanvasMenu.SetActive(false);
+        Time.timeScale = 1f;
         isPaused = false;
+        UIScreen.SetActive(false);
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+        UIScreen.SetActive(true);        
+    }
+    
+    public void GoToMM()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1f;
+    }
+
+    public void LoadAbout()
+    {
+        genScreen.SetActive(false);
+        aboutScreen.SetActive(true);
+    }
+
+    public void LoadControls()
+    {
+        Debug.Log("Show Controls");
+        controlScreen.SetActive(true);
+    }
+
+    public void LoadCredits()
+    {
+        Debug.Log("Show Credits");
+        creditScreen.SetActive(true);
+    }
+
+    public void ReturntoAbout()
+    {
+        controlScreen.SetActive(false);
+        creditScreen.SetActive(false);
+        aboutScreen.SetActive(false);
+    }
+
+    public void ReturntoPause()
+    {
+        aboutScreen.SetActive(false);
+        genScreen.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
     }
 }
