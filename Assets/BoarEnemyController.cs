8f37d82d60b5c1618722aad2e2f0b5f7b05a7f14
poiuyt0418 <poiuyt0418@hotmail.com>
2022-04-17 23:40:34 -0400

    boar ai added to scene
    raycast not working

--- Assets/BoarEnemyController.cs
+++ Assets/BoarEnemyController.cs
@@ -14,6 +14,7 @@ public class BoarEnemyController : MonoBehaviour
     public float randomDistance; // distance the boar chooses to wander towards
     public float aggroRange;
     public GameObject player;
+    Vector3 testPoint = Vector3.zero;
     Animator animator;
     // Start is called before the first frame update
     void Start()
@@ -36,30 +37,41 @@ public class BoarEnemyController : MonoBehaviour
             animator.SetBool("isWalking",true);
         }
         RaycastHit hit;
-        if(Physics.Raycast(transform.position, player.transform.position - transform.position, out hit, aggroRange))
+        if(Physics.Raycast(transform.position, player.transform.position - transform.position, out hit, aggroRange,1))
         {
-            if(enemy.remainingDistance == 0)
+            testPoint = hit.transform.position;
+            if(hit.transform.gameObject.GetComponent(typeof(RaffaController)) != null)
             {
-                enemy.speed = walkSpeed;
-            }
-            if(enemy.speed != chargeSpeed)
-            {
-                enemy.SetDestination(player.transform.position);
-                enemy.speed = chargeSpeed;
-                return;
+                Debug.Log("a");
+                if(enemy.remainingDistance <= .5f)
+                {
+                    enemy.speed = walkSpeed;
+                }
+                if(enemy.speed != chargeSpeed)
+                {
+                    enemy.SetDestination(player.transform.position);
+                    enemy.speed = chargeSpeed;
+                    return;
+                }
             }
         }
         timer += Time.deltaTime;
         if(timer>=walkTime)
         {
-            Debug.Log("1");
             enemy.speed = walkSpeed;
             Vector3 randomPoint = Random.insideUnitSphere * randomDistance;
             randomPoint += origin;
             NavMeshHit realPoint;
-            NavMesh.SamplePosition(randomPoint,out realPoint,randomDistance,1);
+            NavMesh.SamplePosition(randomPoint,out realPoint,randomDistance,-1);
             enemy.SetDestination(realPoint.position);
+            //testPoint = realPoint.position;
             timer = 0;
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(testPoint, 1);
     }
 }
