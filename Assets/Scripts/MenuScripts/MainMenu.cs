4a085a2fd4da6d2c1f927eee3770e3624ad7df0b
JBarrott <jordan.barrott@yahoo.com>
2022-04-26 02:11:05 -0400

    options menu complete

--- Assets/Scripts/MenuScripts/MainMenu.cs
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
@@ -8,13 +10,40 @@ public class MainMenu : MonoBehaviour
     public GameObject aboutScreen;
     public GameObject creditScreen;
     public GameObject controlScreen;
+    public GameObject optionScreen;
     public GameObject genScreen;
-
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
     void Start()
     {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+        
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
         aboutScreen.SetActive(false);
         creditScreen.SetActive(false);
         controlScreen.SetActive(false);
+        optionScreen.SetActive(false);
     }
 
     public void StartGame()
@@ -41,6 +70,12 @@ public class MainMenu : MonoBehaviour
         controlScreen.SetActive(true);
     }
 
+    public void LoadOptionsScreen()
+    {
+        aboutScreen.SetActive(false);
+        optionScreen.SetActive(true);
+    }
+
     public void ReturntoAbout()
     {
         controlScreen.SetActive(false);
@@ -51,7 +86,24 @@ public class MainMenu : MonoBehaviour
     public void ReturntoGenScreen()
     {
         aboutScreen.SetActive(false);
+        optionScreen.SetActive(false);
         genScreen.SetActive(true);
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetFullScreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
     }
 
         public void QuitGame()
