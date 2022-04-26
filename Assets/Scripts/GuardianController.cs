137f07fa42abc4f58d8951baaad206a9124cfed9
JBarrott <jordan.barrott@yahoo.com>
2022-04-25 22:34:09 -0400

    pray for food complete

--- Assets/Scripts/GuardianController.cs
+++ Assets/Scripts/GuardianController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GuardianController : MonoBehaviour
 {
     public bool isAwakened = false;
-
+    public GameObject Ingredient;
     private Animator animator;
 
     void Start()
@@ -18,6 +18,14 @@ public class GuardianController : MonoBehaviour
         Debug.Log("Awakened");
         //start animation or whatever other visual queue
         animator.SetBool("Awake", true);
+        DropIngredient();
         FindObjectOfType<Level2Manager>().areAllGuardiansAwakened();
+    }
+
+    void DropIngredient()
+    {
+        Vector3 position = transform.position;
+        GameObject ingredient = Instantiate(Ingredient, position + new Vector3 (0.0f,3.0f,0.0f), Quaternion.identity);
+        ingredient.SetActive(true);
     }
 }
